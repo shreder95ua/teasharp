820cdf45ff5fb1be5b550615fe219aabe4a854be
tomas <93524600+TommiRommi@users.noreply.github.com>
2022-09-22 10:50:33 +0000

    created the main C file
    this is the teasharp interpretor

---
+++ TS_interpter.c
@@ -0,0 +1,24 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+
+#ifdef __linux
+#include <unistd.h>
+#else
+#include <windows.h>
+#endif
+
+
+int main(int argc, char **argv)
+{
+    /*  this is the start of the teasharp interpretor.
+        It uses gtk for gui capability and is writen in C.
+        There's currently nothing here so add whatever    */ 
+    return(0);
+}
