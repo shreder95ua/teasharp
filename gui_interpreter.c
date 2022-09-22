0ea8e42771e50212737ebf62bbddb7455ccfe90b
tomas <93524600+TommiRommi@users.noreply.github.com>
2022-09-22 11:19:02 +0000

    main gui interpreter

--- gui_interpreter.c
+++ gui_interpreter.c
@@ -7,18 +7,26 @@
 #include <ctype.h>
 #include <math.h>
 
-
 #ifdef __linux
 #include <unistd.h>
 #else
 #include <windows.h>
 #endif
 
+#include "window.c"
 
+/*  this is the start of the teasharp interpretor.
+    It uses gtk for gui capability and is writen in C.
+    feel free to add whatever   */ 
 int main(int argc, char **argv)
 {
-    /*  this is the start of the teasharp interpretor.
-        It uses gtk for gui capability and is writen in C.
-        There's currently nothing here so add whatever    */ 
-    return(0);
+    GtkApplication *app;
+    int status = 0;
+
+    app = gtk_application_new("org.ts.interpreter", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    
+    return(status);
 }
