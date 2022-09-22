25dfe2e8b50f4fe3f9a0ff7fc78d8842d5d71b39
tomas <93524600+TommiRommi@users.noreply.github.com>
2022-09-22 11:43:14 +0000

    middle man processing handler

---
+++ arg_processing.h
@@ -0,0 +1,14 @@
+#ifndef ARGUMENT_PROCESSING
+#define ARGUMENT_PROCESSING
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// everything that needs processing is called through here
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif
