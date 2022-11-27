c5b98f90060d0cf569d6e40d84864c4062a21a9c
Jirayu Kaewsing <strixz.self@gmail.com>
2022-11-27 16:56:50 +0700

    Update the CLI help command

--- cli.py
+++ cli.py
@@ -2,6 +2,15 @@ import click
 from teasharp.interpreter import interpret
 
 @click.command()
-@click.argument('path', type = click.Path(exists = True), required = False)
-def main(path):
+@click.argument('path', type = click.Path(exists = True), metavar = '<PATH_TO_TEASHARP_SCRIPT>', required = False)
+def main(path: str):
+
+    '''
+        Call the Teasharp interpreter.
+
+        If there's no file path specified, it will call the interpreter in REPL mode.
+        
+        If there's a file path, it will interpret that file.
+    '''
+
     interpret(path)
